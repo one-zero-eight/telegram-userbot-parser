b84895570214209fa996a21807f4c32cac9c296e
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-06-22 15:59:09 +0300

    chore: add pre-commit with ruff

--- pyrogram_parser.py
+++ pyrogram_parser.py
@@ -4,15 +4,17 @@ from pydantic import BaseModel
 from datetime import datetime
 
 # File for saving data
-filename='parser.json'
+filename = "parser.json"
 # specify the name of the session file for userbot
 app = Client("my_account")
+
 
 class Chat(BaseModel):
     id: int
     type: str
     title: str
     username: str
+
 
 class StructureMessage(BaseModel):
     id: int
@@ -25,15 +27,16 @@ class StructureMessage(BaseModel):
     def serializableDict(self):
         """Converts an object into a dictionary with date conversion to an ISO format string"""
         new_dict = self.dict()
-        new_dict['date'] = new_dict['date'].isoformat()
+        new_dict["date"] = new_dict["date"].isoformat()
         return new_dict
+
 
 def saveJson(message):
     """Saves the message to a JSON file"""
     message_save = StructureMessage.model_validate_json(message)
 
     # Opening a file and writing data
-    with open(filename, 'r+', encoding='utf-8') as file:
+    with open(filename, "r+", encoding="utf-8") as file:
         try:
             data = json.load(file)
         except json.JSONDecodeError:
@@ -42,13 +45,17 @@ def saveJson(message):
         file.seek(0)
         json.dump(data, file, ensure_ascii=False, indent=4)
 
+
 # Handler for new messages in the specified chats
-@app.on_message(filters.chat(["@sportinIU", "@opportunitiesforyou", "@test_kanal_capstone"]))
+@app.on_message(
+    filters.chat(["@sportinIU", "@opportunitiesforyou", "@test_kanal_capstone"])
+)
 async def new_message_handler(client, message):
     """Processes new messages and saves them if there is a text or signature"""
     if message.text or message.caption:
         message = str(message)
         saveJson(message)
+
 
 if __name__ == "__main__":
     app.run()
